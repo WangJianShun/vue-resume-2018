cc5fa506e37d88d64bbcba38077a9496b6561841
Wang <1779011185@qq.com>
2018-08-19 21:50:47 +0800

    添加login.css

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    editingName: false,
+    resume: {
+      name: '名字',
+      gender: '男',
+      birthday: '1997年1月28日',
+      jobTitle: '前端工程师',
+      email: '1779011185@qq.com',
+      phone: '12345679'
+    }
+  },
+  methods: {
+    onEdit(key, value) {
+      this.resume[key] = value
+    },
+    onClickSave() {
+      var currentUser = AV.User.current();
+      if (currentUser) {
+        // 跳转到首页
+        this.saveResume()
+      }
+      else {
+        this.showLogin()
+        //currentUser 为空时，可打开用户注册界面…
+      }
+      /** 声明类型
+      var User = AV.Object.extend('User');
+      // 新建对象
+      var user = new User();
+      // 设置名称
+      user.set('name', '工作');
+      // 设置优先级
+      user.set('priority', 1);
+      user.save().then(function (todo) {
+        console.log('objectId is ' + todo.id);
+      }, function (error) {
+        console.error(error);
+    });**/
+    }
+  }
+})
---
+++ src/vendors/av.js
@@ -0,0 +1,7 @@
+var APP_ID = 'i6q23SFS8DTF5gEN7WcoUkbm-gzGzoHsz';
+var APP_KEY = 'BTzL4B15BYKOzA4h9A548mkR';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
